dd84b9ec53e8feb59f811d9233000f043ec49b20
TheAndreLuiz <andreluizgaspar@edu.unifil.br>
2024-01-31 09:04:56 -0300

    minor fixes

--- src/finder.js
+++ src/finder.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const fs = require('fs');
 const getDriver = require('./driver.js');
 
 class Finder {
@@ -29,19 +28,20 @@ class Finder {
             const _class = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'class')
             const className = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'className')
             const contentDescription = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'contentDescription')
-            if (text.toString().includes(search) || name.toString().includes(search) || _class.toString().includes(search) || className.toString().includes(search) || contentDescription.toString().includes(search)){
+            if (text.toString().includes(search) || name.toString().includes(search) || _class.toString().includes(search) || className.toString().includes(search) || contentDescription.toString().includes(search)) {
                 fs.appendFileSync('log.txt', JSON.stringify(element) + '  ' + element['element-6066-11e4-a52e-4f735466cecf'] + '\n')
-            const text = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'text')
-            const name = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'name')
-            const _class = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'class')
-            const className = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'className')
-            const contentDescription = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'contentDescription')
-            if (text.toString().includes(search) || name.toString().includes(search) || _class.toString().includes(search) || className.toString().includes(search) || contentDescription.toString().includes(search)){
-                fs.appendFileSync('log.txt', JSON.stringify(element) + '  ' + element['element-6066-11e4-a52e-4f735466cecf'] + '\n')
-                return element['element-6066-11e4-a52e-4f735466cecf']; // avoid magic stuff later
+                const text = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'text')
+                const name = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'name')
+                const _class = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'class')
+                const className = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'className')
+                const contentDescription = await (await getDriver()).getElementAttribute(element['element-6066-11e4-a52e-4f735466cecf'], 'contentDescription')
+                if (text.toString().includes(search) || name.toString().includes(search) || _class.toString().includes(search) || className.toString().includes(search) || contentDescription.toString().includes(search)) {
+                    fs.appendFileSync('log.txt', JSON.stringify(element) + '  ' + element['element-6066-11e4-a52e-4f735466cecf'] + '\n')
+                    return element['element-6066-11e4-a52e-4f735466cecf']; // avoid magic stuff later
+                }
             }
+            return this.waitForElement(search)
         }
-        return this.waitForElement(search)
     }
 
     async findByCss(css) {
